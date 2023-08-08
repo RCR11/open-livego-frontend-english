8d7b72212264d2655f882c43b876d6f9cb064435
RCR11 <123052024+RCR11@users.noreply.github.com>
2023-08-08 11:33:29 -0400

    English Translation

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="mt-2 mb-4 d-flex justify-content-between" id="global_nav">
     <div class="nav">
-      <router-link to="/" class="text-decoration-none "><span :class="{'text-black': !darkMode, 'text-white': darkMode}"><i :class="{'fs-4': true, 'ms-2': true, 'bi': true, 'bi-house-fill': true}"></i> 主页</span></router-link>
-      <span role="button" :class="{'text-white': darkMode, 'ms-md-3': true}" @click="switchDarkMode()"><i :class="{'fs-4': true, 'ms-2': true, 'bi': true, 'bi-brightness-high-fill': !darkMode, 'bi-moon-fill': darkMode }"></i> {{ darkMode ? '深色主题' : '浅色主题' }}</span>
+      <router-link to="/" class="text-decoration-none "><span :class="{'text-black': !darkMode, 'text-white': darkMode}"><i :class="{'fs-4': true, 'ms-2': true, 'bi': true, 'bi-house-fill': true}"></i> Home</span></router-link>
+      <span role="button" :class="{'text-white': darkMode, 'ms-md-3': true}" @click="switchDarkMode()"><i :class="{'fs-4': true, 'ms-2': true, 'bi': true, 'bi-brightness-high-fill': !darkMode, 'bi-moon-fill': darkMode }"></i> {{ darkMode ? 'Dark Mode' : 'Light Mode' }}</span>
     </div>
   </div>
 </template>
