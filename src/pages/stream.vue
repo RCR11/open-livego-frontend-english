bf4808b444efa7d79c79ca1513059135d653ed05
RCR11 <123052024+RCR11@users.noreply.github.com>
2023-08-09 18:02:02 -0400

    Create stream key page

---
+++ src/pages/stream.vue
@@ -0,0 +1,11 @@
+<template>  
+  <div id="app"></div>  
+</template>  
+<script>  
+export default {  
+  name: "App",  
+  mounted() {  
+    document.title = "LiveGo - Stream Here";  
+  },  
+};  
+</script>
